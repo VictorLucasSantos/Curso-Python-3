20bcab68756d3adff3da4ed3dd3b55d46c2daf4e
Victor Guedes <victor.guedes@redeig.com>
2025-06-24 09:24:47 -0400

    Progresso diario 24-06-2025
    aulas 369, 370, 371, 372

--- Secao 07/aula202-calculadora/display.py
+++ Secao 07/aula202-calculadora/display.py
@@ -2,13 +2,15 @@ from PySide6.QtWidgets import QLineEdit
 from PySide6.QtCore import Qt, Signal
 from PySide6.QtGui import QKeyEvent
 from variables import BIG_FONT_SIZE, TEXT_MARGIN, MININUM_WIDTH
-from utils import isEmpty
+from utils import isEmpty, isNumOrDot
 
 
 class Display(QLineEdit):
     eqPressed = Signal()
     delPressed = Signal()
     clearPressed = Signal()
+    inputPressed = Signal(str)
+    operatorPressed = Signal(str)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -29,18 +31,36 @@ class Display(QLineEdit):
         isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
         isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete]
         isEsc = key in [KEYS.Key_Escape]
+        isOperator = key in [
+            KEYS.Key_Plus,
+            KEYS.Key_Slash,
+            KEYS.Key_Minus,
+            KEYS.Key_Asterisk,
+            KEYS.Key_P,
+        ]
 
-        if isEnter:
+        if isEnter or text == "=":
             self.eqPressed.emit()
             return event.ignore()
 
-        if isEsc:
-            self.delPRessed.emit()
+        if isOperator:
+            if text.lower() == "p":
+                text = "^"
+            self.operatorPressed.emit(text)
             return event.ignore()
 
-        if isDelete:
-            self.isDelete.emit()
+        if isEsc:
+            self.clearPressed.emit()
+            return event.ignore()
+
+        if isDelete or text.lower() == "c":
+            self.delPressed.emit()
             return event.ignore()
 
         if isEmpty(text):
+            return event.ignore()
+
+        if isNumOrDot(text):
+            print()
+            self.inputPressed.emit(text)
             return event.ignore()
